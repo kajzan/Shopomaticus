698e7d51643f7abfc2637b588bc968a38a094251
kajzan <k.trojovsky@gmx.at>
2026-06-29 00:15:46 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+// ---------------------------------------------------------------
+// Deine Firebase-Zugangsdaten. Diese Datei legst du EINMAL an und
+// musst sie danach nie wieder anfassen – auch bei neuen Versionen
+// tauschst du nur die index.html aus.
+//
+// Ersetze die sechs Werte unten durch deine eigenen. Am einfachsten:
+// kopiere sie aus deiner bisherigen, funktionierenden index.html
+// (die Zeilen mit apiKey, authDomain, ...) – die sind garantiert korrekt.
+// ---------------------------------------------------------------
+export const firebaseConfig = {
+      apiKey: "AIzaSyAeZKQcXshcliezBHBCt7wRBwROzBrByXQ",
+      authDomain: "shopomatus.firebaseapp.com",
+      projectId: "shopomatus",
+      storageBucket: "shopomatus.firebasestorage.app",
+      messagingSenderId: "292760834395",
+      appId: "1:292760834395:web:995ef2d5bbe0b655dcca7e"
+};
